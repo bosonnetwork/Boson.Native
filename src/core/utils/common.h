17f12e3b90d02608a6d191be6fe7e29874dad2c4
Dongxiao <kuit@msn.com>
2023-09-20 17:25:19 +0800

    fixed utf8proc_NFC memory leak

---
+++ src/core/utils/common.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 trinity-tech.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <utf8proc.h>
+#include <string>
+
+namespace carrier {
+    std::string utf8NFC(const std::string& src) {
+        utf8proc_uint8_t* utf = utf8proc_NFC((unsigned char *)(src.c_str()));
+        std::string ret {};
+        if (utf) {
+            ret = (char*)utf;
+            free(utf);
+        }
+        return ret;
+    }
+}
